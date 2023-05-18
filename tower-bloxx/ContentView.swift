a2d7de7c608bd1b8418af8ba9330b89c40826ab8
keziaglr <keziagloria24@gmail.com>
2023-05-18 14:22:15 +0700

    Initial Commit

---
+++ tower-bloxx/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  tower-bloxx
+//
+//  Created by Kezia Gloria on 18/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tower-bloxx/tower_bloxxApp.swift
@@ -0,0 +1,17 @@
+//
+//  tower_bloxxApp.swift
+//  tower-bloxx
+//
+//  Created by Kezia Gloria on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct tower_bloxxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
